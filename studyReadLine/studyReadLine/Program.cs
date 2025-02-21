b9fe397c66e20267a37e954ef22e34cd85eea60b
overy1234 <overy1234@naver.com>
2025-02-21 16:01:52 +0900

    studyReadLine

---
+++ studyReadLine/studyReadLine/Program.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace studyReadLine
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //사용자 입력을 문자열 받기
+            //Console.Write("이름을 입력하세요: ");
+            //string userName = Console.ReadLine(); //사용자로 부터 입력 받기
+
+            //Console.WriteLine($"안녕하세요, {userName}님!"); 
+
+
+            //문자열을 정수로 변환
+            //Console.Write("나이를 입력하세요: ");
+            //string input = Console.ReadLine(); //사용자로부터 입력 받기
+            //int age = int.Parse(input); //문자열을 정수로 변환
+
+            //Console.WriteLine($"내년에는 {age + 1} 살이 되겠군요!"); //변환된 값 사용
+            //Console.WriteLine("내년에는 " + age + "살이 되겠군요!");
+            //Console.WriteLine("내년에는 {0} 살이 되겠군요!", age);
+
+
+            //해당 스샷내용 입력받아서 출력
+            //활동
+            //루인 스킬 피해           : 4.5<-입력받기
+            //카드 게이지 획득량
+            //각성기 피해
+            //최대 마나
+            //전투 중 마나 회복량
+            //비전투 중 마나 회복량
+            //이동 속도
+            //탈 것 속도
+            //운반 속도
+            //스킬 재사용 대기시간 감소 
+
+
+            //float Skill_Damage = 0.0f;
+            //float Card_Gage = 0.0f;
+            //float AwakeDamage = 0.0f;
+            //int Max_Mana = 0;
+            //int CombatManaRegen;
+            //int manaRegenOutOfCombat;
+            //float moveSpeed;
+            //float rideSpeed;
+            //float carrySpeed;
+            //float cooldownReduction;
+            //Console.Write("스킬 피해량을 입력하세요 : ");
+            //Skill_Damage = float.Parse(Console.ReadLine());
+            //Console.Write("스킬 피해량을 입력하세요 : ");
+            //Card_Gage = float.Parse(Console.ReadLine());
+            //Console.Write("각성기 피해 : ");
+            //AwakeDamage = float.Parse(Console.ReadLine());
+            //Console.Write("최대마나 : ");
+            //Max_Mana = int.Parse(Console.ReadLine());
+            //Console.Write("전투 중 마나 회복량 : ");
+            //CombatManaRegen = int.Parse(Console.ReadLine());
+            //Console.Write("비전투 중 마나 회복량  : ");
+            //manaRegenOutOfCombat = int.Parse(Console.ReadLine());
+            //Console.Write("이동 속도  : ");
+            //moveSpeed = float.Parse(Console.ReadLine());
+            //Console.Write("탈 것 속도  : ");
+            //rideSpeed = float.Parse(Console.ReadLine());
+            //Console.Write("운반 속도  : ");
+            //carrySpeed = float.Parse(Console.ReadLine());
+            //Console.Write("스킬 재사용 대기시간 감소   : ");
+            //cooldownReduction = float.Parse(Console.ReadLine());
+
+
+            //Console.WriteLine($"루인 스킬 피해                    : {Skill_Damage}%");
+            //Console.WriteLine($"카드 게이지 획득량                 : {Card_Gage}%");
+            //Console.WriteLine($"각성기 피해                       : {AwakeDamage}%");
+            //Console.WriteLine($"최대마나                          : {Max_Mana}");
+            //Console.WriteLine($"전투 중 마나 회복량                : {CombatManaRegen}");
+            //Console.WriteLine($"비전투 중 마나 회복량              : {manaRegenOutOfCombat}");
+            //Console.WriteLine($"이동 속도                         : {moveSpeed}%");
+            //Console.WriteLine($"탈것 속도                         : {rideSpeed}%");
+            //Console.WriteLine($"운반 속도                         : {carrySpeed}%");
+            //Console.WriteLine($"스킬 재사용 대기시간 감소           : {cooldownReduction}%");
+
+
+
+
+
+
+        }
+    }
+}
