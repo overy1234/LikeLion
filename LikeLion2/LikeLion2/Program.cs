3bc86072d4ec791c8a661fa902a4d991145a746d
overy1234 <overy1234@naver.com>
2025-02-21 10:22:55 +0900

    study2

---
+++ LikeLion2/LikeLion2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LikeLion2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //변수선언
+            int age; //정수형 변수
+            age = 10; //변수에 값 저장
+
+            Console.WriteLine(age); //변수에 저장된 값을 출력  25
+
+            //컨트롤+s
+
+        }
+    }
+}
