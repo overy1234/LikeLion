7e6f69cb284dbc3dc488fb34dfde528d7091f6b3
overy1234 <overy1234@naver.com>
2025-02-21 17:40:42 +0900

    todayTask

---
+++ TodayTask1/TodayTask1/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace TodayTask1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //로딩바 시작화면
+            //겜임스토리1
+            //비주얼노벨만들어보기
+            Console.WriteLine("┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓");
+            Thread.Sleep(1000);
+            Console.WriteLine("┃                                              ┃");
+            Thread.Sleep(1000);
+            Console.WriteLine("┃                                              ┃");
+            Thread.Sleep(1000);
+            Console.WriteLine("┃           인   디   월   드                  ┃");
+            Thread.Sleep(1000);
+            Console.WriteLine("┃                                              ┃");
+            Thread.Sleep(1000);
+            Console.WriteLine("┃                                              ┃");
+            Thread.Sleep(1000);
+            Console.WriteLine("┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛");
+            Console.WriteLine("로딩바 엔터를 치면 스토리가 시작됩니다. ");
+            Console.ReadLine();
+            Console.Clear();
+            Console.WriteLine("인디월드시작되었다.");
+            Console.ReadLine();
+            Console.Clear();
+            Console.WriteLine("100명 생존을 위해 싸움이 시작된다.");
+            Console.ReadLine();
+            Console.Clear();
+            Console.WriteLine("\"인디월드\"는 단순한 생존 게임이 아니다. 어느 날, 100명의 플레이어가 정체불명의 가상 세계로 소환된다. 그들은 이곳이 단순한 시뮬레이션이 아니라 현실과 연결된 미스터리한 공간임을 알게 된다. 생존을 위해 자원을 모으고, 동맹을 맺거나 배신하며, 끊임없이 변화하는 환경에서 살아남아야 한다. 이 세계의 비밀을 밝혀내고, 최후의 1인이 될 수 있을 것인가?\"");
+
+            Console.ReadLine();
+            Console.Clear();
+            Console.WriteLine("날씨가 추워지고있다. 추위를 피할 식량과 집 불을 먼저 얻기위해 이동을 해야할거같다.");
+            Console.ReadLine();
+            Console.Clear();
+            Console.WriteLine("앞에 사람이 보인다. 여긴 서바이벌 게임이다 생존을위해 말을 걸어야할까? 혼자 생존을위해" +
+               "   말을 걸어본다? 1, 다른곳으로 간다? 2");
+            Console.ReadLine();
+            Console.Clear();
+            Console.WriteLine("말을 걸어봤다.  You Die");
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
