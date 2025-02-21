d6d03d414c759239d66a0726a4d89033b4e20c9f
overy1234 <overy1234@naver.com>
2025-02-21 13:07:18 +0900

    study4

---
+++ Study4/Study4/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Study4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ////문자열 
+            //string greeting;  //문자열 변수를 선언
+            //greeting = "Hello, World!";  //변수에 값을 저장
+
+            ////변수의 값을 사용
+            //Console.WriteLine(greeting); //출력 : Hello , World!
+
+
+            //변수 선언과 초기화를 한번에 수행
+            //int score = 100; //정수형 변수 선언과 동시에 100으로 초기화
+            //double temperature = 36.5; // 실수형 변수 선언과 초기화
+            //string city = "Seoul"; //문자열 변수 선언과 초기화 
+
+
+            ////변수 출력
+            //Console.WriteLine(score); //출력 100
+            //Console.WriteLine(temperature); //출력 36.5
+            //Console.WriteLine(city); //출력 : Seoul 
+
+
+            //같은 데이터 타입의 변수를 쉼표로 구분해서 선언
+            //int x = 10, y = 20, z = 30;   //정수형 변수 x,y,z 를 선언하고 초기화
+
+
+            //Console.WriteLine(x);
+            //Console.WriteLine(y);
+            //Console.WriteLine(z);
+
+
+            //const double Pi = 3.14159; //상수 pi선언 및 초기화
+            //const int MaxScore = 100; //정수형 상수 선언
+
+           
+            ////출력
+            //Console.WriteLine("Pi : " + Pi); //출력 : Pi: 3.14159
+            //Console.WriteLine("Max Score: " + MaxScore); //출력 : Max Score:100
+
+            //
+
+            //백준
+            //프로그래머스  
+            //3단계이상풀어야된다. 대기업 
+            //동기부여 
+
+
+
+        }
+    }
+}
---
+++ study3/study3/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace study3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //캐릭터 
+            //hp : 100
+            //att : 56.7
+            //캐릭터이름 : ??
+            //등급 : S
+
+            int hp = 100;
+            double att = 56.7;
+            string name = "카타리나";
+            char grade = 'S';
+
+
+            Console.WriteLine("캐릭터");
+            Console.WriteLine("공격력 : "+att);
+            Console.WriteLine("체력 : "+hp);
+            Console.WriteLine("캐릭터이름 : "+name);
+            Console.WriteLine("등급 : "+grade);
+            
+           
+
+        }
+    }
+}
