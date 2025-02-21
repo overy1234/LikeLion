a939f308d53679d5dd1e379fee2ef314290d516a
overy1234 <overy1234@naver.com>
2025-02-21 15:04:07 +0900

    loading

---
+++ Loading/Loading/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Loading
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+
+
+
+
+            Console.WriteLine("□□□□□□□□□□");
+            Thread.Sleep(2000); //1000 1초다 (1000밀리초)
+            Console.Clear(); //콘솔 화면 지우기
+            Console.WriteLine("■□□□□□□□□□");
+            Thread.Sleep(2000); //1000 1초다 (1000밀리초)
+            Console.Clear(); //콘솔 화면 지우기
+            Console.WriteLine("■■□□□□□□□□");
+            Thread.Sleep(2000); //1000 1초다 (1000밀리초)
+            Console.Clear(); //콘솔 화면 지우기
+            Console.WriteLine("■■■□□□□□□□");
+            Thread.Sleep(2000); //1000 1초다 (1000밀리초)
+            Console.Clear(); //콘솔 화면 지우기
+            Console.WriteLine("■■■■□□□□□□");
+            Thread.Sleep(2000); //1000 1초다 (1000밀리초)
+            Console.Clear(); //콘솔 화면 지우기
+            Console.WriteLine("■■■■■□□□□□");
+            Thread.Sleep(2000); //1000 1초다 (1000밀리초)
+            Console.Clear(); //콘솔 화면 지우기
+            Console.WriteLine("■■■■■■□□□□");
+            Thread.Sleep(2000); //1000 1초다 (1000밀리초)
+            Console.Clear(); //콘솔 화면 지우기
+            Console.WriteLine("■■■■■■■□□□");
+            Thread.Sleep(2000); //1000 1초다 (1000밀리초)
+            Console.Clear(); //콘솔 화면 지우기
+            Console.WriteLine("■■■■■■■■□□");
+            Thread.Sleep(2000); //1000 1초다 (1000밀리초)
+            Console.Clear(); //콘솔 화면 지우기
+            Console.WriteLine("■■■■■■■■■□");
+            Thread.Sleep(2000); //1000 1초다 (1000밀리초)
+            Console.Clear(); //콘솔 화면 지우기
+            Console.WriteLine("■■■■■■■■■■");
+            Console.Clear(); //콘솔 화면 지우기
+            Console.WriteLine("텍스트엘든링");
+
+            Console.WriteLine("스토리1");
+
+
+
+        }
+    }
+}
