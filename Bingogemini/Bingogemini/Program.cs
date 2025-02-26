f8bb55e61bc57c528ed7c9530f5d98d46e83b03c
overy1234 <overy1234@naver.com>
2025-02-27 08:59:32 +0900

    bingo1

---
+++ Bingogemini/Bingogemini/Program.cs
@@ -0,0 +1,287 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class BingoGame
+{
+    private int[,] card;
+    private List<int> drawnNumbers;
+    private Random random;
+    private bool[,] marked; // 맞춘 숫자 표시를 위한 2차원 배열
+
+    public BingoGame()
+    {
+        card = new int[5, 5];
+        drawnNumbers = new List<int>();
+        random = new Random();
+        marked = new bool[5, 5]; // marked 배열 초기화
+    }
+
+    public void GenerateCard()
+    {
+        // 각 열에 맞는 숫자 범위로 숫자 풀 생성
+        List<int>[] columnNumberPools = new List<int>[5];
+        int[] startNumbers = { 1, 16, 31, 46, 61 };
+        int[] endNumbers = { 15, 30, 45, 60, 75 };
+
+        for (int i = 0; i < 5; i++)
+        {
+            columnNumberPools[i] = Enumerable.Range(startNumbers[i], endNumbers[i] - startNumbers[i] + 1).ToList();
+            Shuffle(columnNumberPools[i]); // 각 숫자 풀을 섞음
+        }
+
+        // 빙고 카드에 숫자 할당
+        for (int col = 0; col < 5; col++)
+        {
+            for (int row = 0; row < 5; row++)
+            {
+                if (row == 2 && col == 2) // 가운데는 FREE SPACE
+                {
+                    card[row, col] = 0; // 0은 FREE SPACE를 나타냄
+                }
+                else
+                {
+                    // 각 열의 숫자 풀에서 숫자 선택
+                    card[row, col] = columnNumberPools[col][row];
+                }
+            }
+        }
+    }
+
+    private void Shuffle<T>(List<T> list)
+    {
+        int n = list.Count;
+        for (int i = n - 1; i > 0; i--)
+        {
+            int j = random.Next(i + 1);
+            T temp = list[i];
+            list[i] = list[j];
+            list[j] = temp;
+        }
+    }
+
+    public int DrawNumber()
+    {
+        int number;
+        do
+        {
+            number = random.Next(1, 76); // 1부터 75까지 숫자
+        } while (drawnNumbers.Contains(number));
+        drawnNumbers.Add(number);
+        return number;
+    }
+
+    public bool CheckBingo()
+    {
+        // 가로 빙고 확인
+        for (int row = 0; row < 5; row++)
+        {
+            int[] rowLine = GetRow(row);
+            Console.WriteLine($"가로 줄 {row + 1}: {string.Join(", ", rowLine)}"); // 디버깅 출력
+            if (CheckLine(rowLine, row, true)) // CheckLine에 row, isRow=true 추가
+            {
+                Console.WriteLine($"가로 줄 {row + 1} 빙고!"); // 디버깅 출력
+                return true;
+            }
+        }
+
+        // 세로 빙고 확인
+        for (int col = 0; col < 5; col++)
+        {
+            int[] colLine = GetColumn(col);
+            Console.WriteLine($"세로 줄 {col + 1}: {string.Join(", ", colLine)}"); // 디버깅 출력
+            if (CheckLine(colLine, col, false)) // CheckLine에 col, isRow=false 추가
+            {
+                Console.WriteLine($"세로 줄 {col + 1} 빙고!"); // 디버깅 출력
+                return true;
+            }
+        }
+
+        // 대각선 빙고 확인 (왼쪽 위에서 오른쪽 아래)
+        int[] diagonal1Line = GetDiagonal1();
+        Console.WriteLine($"대각선 줄 1: {string.Join(", ", diagonal1Line)}"); // 디버깅 출력
+        if (CheckLine(diagonal1Line, -1, true, true)) // CheckLine에 isDiagonal1=true 추가
+        {
+            Console.WriteLine($"대각선 줄 1 빙고!"); // 디버깅 출력
+            return true;
+        }
+
+        // 대각선 빙고 확인 (오른쪽 위에서 왼쪽 아래)
+        int[] diagonal2Line = GetDiagonal2();
+        Console.WriteLine($"대각선 줄 2: {string.Join(", ", diagonal2Line)}"); // 디버깅 출력
+        if (CheckLine(diagonal2Line, -1, false, true)) // CheckLine에 isDiagonal2=true 추가
+        {
+            Console.WriteLine($"대각선 줄 2 빙고!"); // 디버깅 출력
+            return true;
+        }
+
+        return false;
+    }
+
+    private bool CheckLine(int[] line, int lineIndex, bool isRow, bool isDiagonal = false) // CheckLine 수정
+    {
+        Console.WriteLine($"CheckLine 입력: {string.Join(", ", line)}"); // 디버깅 출력
+        for (int i = 0; i < line.Length; i++)
+        {
+            int number = line[i];
+            int row = isRow ? lineIndex : i;
+            int col = isRow ? i : lineIndex;
+            if (isDiagonal)
+            {
+                if (isRow) // isRow가 대각선1 나타냄
+                {
+                    row = i;
+                    col = i;
+                }
+                else // isRow가 false면 대각선2 나타냄
+                {
+                    row = i;
+                    col = 4 - i;
+                }
+            }
+
+            if (number != 0 && !marked[row, col]) // 0은 FREE SPACE, marked 배열 확인
+            {
+                Console.WriteLine($"CheckLine 결과: 빙고 아님"); // 디버깅 출력
+                return false;
+            }
+        }
+        Console.WriteLine($"CheckLine 결과: 빙고!"); // 디버깅 출력
+        return true;
+    }
+
+    private int[] GetRow(int row)
+    {
+        int[] line = new int[5];
+        for (int col = 0; col < 5; col++)
+        {
+            line[col] = card[row, col];
+        }
+        return line;
+    }
+
+    private int[] GetColumn(int col)
+    {
+        int[] line = new int[5];
+        for (int row = 0; row < 5; row++)
+        {
+            line[row] = card[row, col];
+        }
+        return line;
+    }
+
+    private int[] GetDiagonal1() // 왼쪽 위에서 오른쪽 아래
+    {
+        return new int[] { card[0, 0], card[1, 1], card[2, 2], card[3, 3], card[4, 4] };
+    }
+
+    private int[] GetDiagonal2() // 오른쪽 위에서 왼쪽 아래
+    {
+        return new int[] { card[0, 4], card[1, 3], card[2, 2], card[3, 1], card[4, 0] };
+    }
+
+    public void PrintCard()
+    {
+        Console.WriteLine("빙고 카드:");
+        Console.WriteLine("--------------------");
+        Console.WriteLine(" B  I  N  G  O");
+        Console.WriteLine("--------------------");
+        for (int row = 0; row < 5; row++)
+        {
+            Console.Write(" ");
+            for (int col = 0; col < 5; col++)
+            {
+                if (row == 2 && col == 2)
+                {
+                    Console.Write("FREE ");
+                }
+                else
+                {
+                    if (marked[row, col]) // 맞춘 숫자일 경우 'X' 표시
+                    {
+                        Console.Write(" X  ");
+                    }
+                    else
+                    {
+                        Console.Write(string.Format("{0,2} ", card[row, col]));
+                    }
+                }
+            }
+            Console.WriteLine();
+        }
+        Console.WriteLine("--------------------");
+    }
+
+    public void PrintDrawnNumbers()
+    {
+        Console.WriteLine("뽑힌 숫자:");
+        foreach (int number in drawnNumbers)
+        {
+            Console.Write(number + " ");
+        }
+        Console.WriteLine();
+    }
+
+    public bool MarkNumber(int number) // 숫자 맞추기 메서드
+    {
+        for (int row = 0; row < 5; row++)
+        {
+            for (int col = 0; col < 5; col++)
+            {
+                if (card[row, col] == number)
+                {
+                    marked[row, col] = true; // 숫자 맞춤 표시
+                    drawnNumbers.Add(number); // 뽑힌 숫자 목록에 추가 (빙고 체크를 위해)
+                    return true; // 숫자 찾음
+                }
+            }
+        }
+        return false; // 숫자 못 찾음
+    }
+}
+
+public class Bingo
+{
+    public static void Main(string[] args)
+    {
+        BingoGame game = new BingoGame();
+        game.GenerateCard();
+        game.PrintCard();
+
+        while (!game.CheckBingo())
+        {
+            Console.WriteLine("숫자를 입력하세요 (1-75):");
+            string input = Console.ReadLine();
+            if (int.TryParse(input, out int number))
+            {
+                if (number >= 1 && number <= 75)
+                {
+                    if (game.MarkNumber(number))
+                    {
+                        Console.WriteLine($"숫자 {number}를 맞췄습니다!");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"카드에 숫자 {number}가 없습니다.");
+                    }
+                    game.PrintCard(); // 매번 카드 상태 출력
+                    game.PrintDrawnNumbers(); // 뽑힌 숫자 목록 출력 (선택 사항)
+
+                    if (game.CheckBingo())
+                    {
+                        Console.WriteLine("빙고!");
+                        break;
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("1부터 75 사이의 숫자를 입력하세요.");
+                }
+            }
+            else
+            {
+                Console.WriteLine("잘못된 입력입니다. 숫자를 입력하세요.");
+            }
+        }
+    }
+}
---
+++ Study10/Study10/Program.cs
@@ -0,0 +1,191 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Study10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //배열 
+            // 0부터시작한다.
+
+            //int[] num = new int[3];  //3개 메모리 만들겠다.
+
+            //num[0] = 10;
+            //num[1] = 20;
+            //num[2] = 30;
+
+
+            //for(int i =0; i<3; i++)
+            //{
+            //    Console.WriteLine(num[i]);
+            //}
+
+            //int[] numbers = { 1, 2,3}; //간단한 선언과 초기화
+            //int[] numbers2 = new int[3];  //크기만 지정
+            //int[] numbers3 = new int[] { 1, 2,3}; //초기화와 함께 선언
+
+
+            //for(int i =0; i<3; i++)
+            //{
+            //    Console.WriteLine(numbers3[i]);
+            //}
+
+
+            //string[] fruits = { "사과", "바나나", "오렌지" };
+
+            //for(int i =0; i<3; i++)
+            //{
+            //    Console.WriteLine(fruits[i]);
+            //}
+
+            //3명의 
+            //국어, 영어, 수학 점수를 입력받고
+            //총점과 평균을 출력하세요.
+
+            //int[] iKor = new int[3];
+            //int[] iEng = new int[3];
+            //int[] iMath = new int[3];
+
+            //int[] sum = new int[3];
+            //float[] aver = new float[3];
+
+
+            ////학생입력받기
+
+            //for(int i =0; i<3; i++)
+            //{
+            //    Console.WriteLine("학생 성적입력하세요 : ");
+            //    Console.Write("국어 : ");
+            //    iKor[i] = int.Parse(Console.ReadLine());
+            //    Console.Write("영어 : ");
+            //    iEng[i] = int.Parse(Console.ReadLine());
+            //    Console.Write("수학 : ");
+            //    iMath[i] = int.Parse(Console.ReadLine());
+
+
+            //    sum[i] = iKor[i] + iEng[i] + iMath[i];
+
+            //    aver[i] = (float)sum[i] / 3;
+
+
+
+            //}
+
+
+
+            ////출력
+            //for(int i=0; i<3; i++)
+            //{
+            //    Console.WriteLine((i+1)+"번학생");
+            //    Console.WriteLine($"국어 : {iKor[i]} 영어 : {iEng[i]} 수학 : {iMath[i]} ");
+            //    Console.WriteLine("총점 : " + sum[i]);
+            //    Console.WriteLine("평균 : " + aver[i].ToString("F2"));
+            //}
+
+
+            //1차원 배열
+            //int[] scores = new int[3];
+
+            //scores[0] = 90;
+            //scores[1] = 85;
+            //scores[2] = 88;
+
+
+
+            //for(int i =0; i<scores.Length; i++)
+            //{
+            //    Console.WriteLine($"점수 {i + 1}:{scores[i]}");
+            //}
+
+
+            //double value = 123.456789;
+            ////소수점 자릿수 설정하는 포맷
+            //Console.WriteLine(value.ToString("F2"));
+            //Console.WriteLine($"소수점둘째자리 : {value:F2} ");
+            //Console.WriteLine(String.Format("수소점 둘째 자리: {0:F2}",value));
+            ////소수점 없이 정수 출력
+            //Console.WriteLine(value.ToString("F0"));
+
+
+            //double value = 123124124.12323;
+
+            //Console.WriteLine(value.ToString("N2"));
+
+
+            //2차원 배열 선언
+            //int[,] matrix = new int[2, 3]{{ 1,2,3},{ 4,5,6}};
+
+
+
+            //for(int i=0; i<2; i++)
+            //{
+
+            //    for(int j=0; j<3; j++)
+            //    {
+            //        Console.Write($"{matrix[i, j]}");
+            //    }
+            //    Console.WriteLine();
+            //}
+            //
+
+            //int[][] matrix = new int[2][];
+
+            //matrix[0] = new int[3];
+            //matrix[1] = new int[3];
+
+
+            ////값 입력
+            //matrix[0][0] = 1;
+            //matrix[0][1] = 2;
+            //matrix[0][2] = 3;
+
+            //matrix[1][0] = 4;
+            //matrix[1][1] = 5;
+            //matrix[1][2] = 6;
+
+
+            ////출력
+            //for (int i = 0; i < matrix.Length; i++)
+            //{
+            //    for (int j = 0; j < matrix[i].Length; j++)
+            //    {
+            //        Console.Write(matrix[i][j] + " ");
+            //    }
+            //    Console.WriteLine();
+            //}
+
+
+            //6.가변배열 
+            //Console.WriteLine("가변 배열");
+            //int[][] jaggedArray = new int[3][];
+
+            //jaggedArray[0] = new int[] { 1, 2 };
+            //jaggedArray[1] = new int[] { 3, 4, 5 };
+            //jaggedArray[2] = new int[] { 6 };
+
+
+            //for(int i=0; i<jaggedArray.Length; i++)
+            //{
+            //    for(int j=0; j < jaggedArray[i].Length; j++)
+            //    {
+            //        Console.Write($"{jaggedArray[i][j]}");
+            //    }
+            //    Console.WriteLine();
+            //}
+
+            //Console.WriteLine("var 키워드 사용");
+            //var numbers = new[] { 1, 2, 3, 4, 5 };
+            //Console.WriteLine($"배열 타입: {numbers.GetType()}");
+
+
+
+
+
+        }
+    }
+}
