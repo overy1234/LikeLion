de4c416700c04f9ffa23cb4bc7c692b3c2e13ba0
overy1234 <overy1234@naver.com>
2025-03-07 13:49:52 +0900

    brickgame2

--- BrickGame/BrickGame/BALLDATA.cs
+++ BrickGame/BrickGame/BALLDATA.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace BrickGame
 {
-    class BALLDATA
+    public class BALLDATA
     {
         public int nReady; //공움직일 준비상태
         public int nDirect; //공 방향 변수
--- BrickGame/BrickGame/GameManager.cs
+++ BrickGame/BrickGame/GameManager.cs
@@ -10,7 +10,7 @@ namespace BrickGame
     class GameManager
     {
         Ball m_pBall = null;
-        
+        Bar m_pBar = null;
         public void Initialize()
         {
             if(m_pBall == null)
@@ -18,22 +18,39 @@ namespace BrickGame
                 m_pBall = new Ball();
                 m_pBall.Initialize();
             }
+
+            //바
+            if(m_pBar == null)
+            {
+                m_pBar = new Bar();
+                m_pBar.Initialize();
+
+            }
+
+            //볼에서 바와 벽돌을 사용해야할거같다.
+            m_pBall.SetBar(m_pBar);
+
+
         }
 
         public void Progress()
         {
             m_pBall.Progress();
+            m_pBar.Progress(ref m_pBall);
+
         }
 
         public void Render()
         {
             Console.Clear();
             m_pBall.Render();
+            m_pBar.Render();
         }
 
         public void Release()
         {
             m_pBall.Release();
+            m_pBar.Release();
         }
 
     }
